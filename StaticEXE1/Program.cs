dd945ea69f5f3396e7ce7f002ef5f0db6d6270d2
Forrest Rabideau <forrest.rabideau@gmail.com>
2022-02-03 14:50:54 -0800

    first commit

---
+++ StaticEXE1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StaticEXE1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            var fahCon = TempConverter.FahrenheitToCelsius(90);
+            var celCon = TempConverter.CelsiusToFahrenheit(30);
+
+            //0:0.0 formats output to display 1 place right of decimal
+            Console.WriteLine($"Fahrenhit to Celsius {fahCon, 0:0.0}");
+            Console.WriteLine($"Celsius to Fahrenhit {celCon, 0:0.0}");
+            
+        }
+    }
+}
---
+++ StaticEXE1/TempConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticEXE1
+{
+   public static class TempConverter
+    {
+        // formula found at https://www.almanac.com/temperature-conversion-celsius-fahrenheit
+        public static double FahrenheitToCelsius(double f)
+    {
+        double ConSum = (f -32) * .5556;
+            
+        return ConSum;
+    }
+        //convert Cel to Fah
+  public static double CelsiusToFahrenheit(double c)
+    {
+        double ConSum = (c * 1.8) + 32;
+            
+        return ConSum;
+    }
+
+    }
+
+    
+
+  
+}
